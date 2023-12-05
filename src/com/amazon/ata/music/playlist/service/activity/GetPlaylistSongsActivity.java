4eda03be82957e243c80c07021c1973fd59d1c64
maddiodie <maddiodie@gmail.com>
2023-12-04 20:02:14 -0700

    finished milestone 2 ... pushing to codegrade to see if it passes

--- src/com/amazon/ata/music/playlist/service/activity/GetPlaylistSongsActivity.java
+++ src/com/amazon/ata/music/playlist/service/activity/GetPlaylistSongsActivity.java
@@ -1,5 +1,8 @@
 package com.amazon.ata.music.playlist.service.activity;
 
+import com.amazon.ata.music.playlist.service.converters.ModelConverter;
+import com.amazon.ata.music.playlist.service.dynamodb.models.AlbumTrack;
+import com.amazon.ata.music.playlist.service.dynamodb.models.Playlist;
 import com.amazon.ata.music.playlist.service.models.requests.GetPlaylistSongsRequest;
 import com.amazon.ata.music.playlist.service.models.results.GetPlaylistSongsResult;
 import com.amazon.ata.music.playlist.service.models.SongModel;
@@ -11,7 +14,9 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import javax.inject.Inject;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
 /**
  * Implementation of the GetPlaylistSongsActivity for the MusicPlaylistService's GetPlaylistSongs API.
@@ -34,6 +39,13 @@ public class GetPlaylistSongsActivity implements RequestHandler<GetPlaylistSongs
         this.playlistDao = playlistDao;
     }
 
+//    public GetPlaylistSongsActivity() {
+//        AmazonDynamoDB amazonDynamoDB = DynamoDbClientProvider.getDynamoDBClient();
+//        DynamoDBMapper dynamoDBMapper = new DynamoDBMapper(amazonDynamoDB);
+//        this.playlistDao = new PlaylistDao(dynamoDBMapper);
+//    }
+    // strictly for the aws lambda function ... requires default no-argument constructor
+
     /**
      * This method handles the incoming request by retrieving the playlist from the database.
      * <p>
@@ -48,11 +60,21 @@ public class GetPlaylistSongsActivity implements RequestHandler<GetPlaylistSongs
     @Override
     public GetPlaylistSongsResult handleRequest(final GetPlaylistSongsRequest getPlaylistSongsRequest,
                                                 Context context) {
-
         log.info("Received GetPlaylistSongsRequest {}", getPlaylistSongsRequest);
 
+        String requestedId = getPlaylistSongsRequest.getId();
+        Playlist playlist = playlistDao.getPlaylist(requestedId);
+
+        List<SongModel> songModelList = new ArrayList<>();
+
+        for (AlbumTrack albumTrack : playlist.getSongList()) {
+            SongModel songModel = new ModelConverter().toSongModel(albumTrack);
+
+            songModelList.add(songModel);
+        }
+
         return GetPlaylistSongsResult.builder()
-                .withSongList(Collections.singletonList(new SongModel()))
+                .withSongList(songModelList)
                 .build();
     }
 
